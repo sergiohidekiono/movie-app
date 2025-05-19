d4b7a5bafa3c1f06c65117819908f93b2fde0f9a
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2025-05-19 00:06:58 -0300

    Mostrando os Filmes em Altas.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,9 +14,17 @@ import {
     View,
 } from "react-native";
 import "../globals.css";
+import { getTrendingMovies } from "@/services/appwrite";
+import TrendingCard from "@/components/TrendingCard";
 
 export default function Index() {
     const router = useRouter();
+
+    const {
+        data: trendingMovies,
+        loading: trendingLoading,
+        error: trendingError,
+    } = useFetch(getTrendingMovies);
 
     const {
         data: movies,
@@ -37,23 +45,44 @@ export default function Index() {
                     className="w-12 h-10 mt-20 mb-5 mx-auto"
                 />
 
-                {moviesLoading ? (
+                {moviesLoading || trendingLoading ? (
                     <ActivityIndicator
                         size="large"
                         color="#0000ff"
                         className="mt-10 self-center"
                     />
-                ) : moviesError ? (
-                    <Text>Error: {moviesError.message}</Text>
+                ) : moviesError || trendingError ? (
+                    <Text>
+                        Error: {moviesError?.message || trendingError?.message}
+                    </Text>
                 ) : (
                     <View className="flex-1 mt-5">
                         <SearchBar
                             onPress={() => router.push("/search")}
-                            value=""
-                            onChangeText={() => {}}
                             placeholder="Search for a movie"
                         />
+                        {trendingMovies && (
+                            <View className="mt-10">
+                                <Text className="text-lg text-white font-bold mb-3">
+                                    Trending Movies
+                                </Text>
+                            </View>
+                        )}
                         <>
+                            <FlatList
+                                horizontal
+                                showsHorizontalScrollIndicator={false}
+                                ItemSeparatorComponent={() => (
+                                    <View className="w-4" />
+                                )}
+                                className="mb-4 mt-3"
+                                data={trendingMovies}
+                                renderItem={({ item, index }) => (
+                                    <TrendingCard movie={item} index={index} />
+                                )}
+                                keyExtractor={(item, index) => index.toString()}
+                            />
+
                             <Text className="text-lg text-white font-bold mt-5 mb-3">
                                 Latest Movies
                             </Text>
