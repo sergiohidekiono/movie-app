d973e75a08003a855bee84164a1ae266301089af
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2025-05-19 00:41:46 -0300

    dando uma cara pra area de filmes salvos e perfil do usuário.

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import { icons } from "@/constants/icons";
 
 const saved = () => {
     return (
-        <View>
-            <Text>saved</Text>
+        <View className="bg-primary flex-1 px-10">
+            <View className="flex justify-center items-center flex-1 flexc-col gap-5">
+                <Image source={icons.save} />
+                <Text className="text-gray-500 text-base">Save</Text>
+            </View>
         </View>
     );
 };
